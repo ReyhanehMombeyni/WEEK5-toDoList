bbc6865e178f28befcd435f31f328ba56dae69f1
Reyhaneh <reyhaneh.sa.mo@gmail.com>
2025-02-18 21:49:24 +0330

    add edit button and style

--- src/components/unshared/Home.jsx
+++ src/components/unshared/Home.jsx
@@ -5,6 +5,8 @@ import Items from "./Items"
 
 const Home = () => {
   const [items, setItems]= useState([]);
+  const [textEdit, setTextEdit]= useState("");
+  const [isEdit, setIsEdit]= useState(null)
 
   const getData= async () => {
     const res= await axios.get('https://dummyjson.com/todos')
@@ -15,6 +17,10 @@ const Home = () => {
     getData()
     
   }, [])
+  useEffect(() => {
+    const temp = JSON.stringify(items);
+    localStorage.setItem("todos", temp);
+  },[items])
 
   const deleteHandler = (id) => {
     const newItems= items.filter(item => item.id !== id);
@@ -22,8 +28,25 @@ const Home = () => {
   }
 
   const editHandler = (id) => {
-    console.log(id);
-    
+    const editItems= items.filter(item => {
+      if(item.id=== id) {
+        item.todo= textEdit;
+      }
+      return item;
+  });
+    setItems(editItems)
+    setIsEdit(null)
+    setTextEdit("")
+  }
+
+  const checkHandler = (id) =>  {
+    const checkItems= items.filter(item => {
+      if(item.id=== id) {
+        item.completed =!item.completed;
+      }
+      return item;
+  });
+    setItems(checkItems)
   }
 
 
@@ -33,7 +56,7 @@ const Home = () => {
     <div className="flex flex-col">
         <h1 className="text-4xl font-bold pb-10">toDo<span className="text-blue-400">List</span></h1>
         <Add items={items} setItems={setItems} />
-        <Items items={items} deleteHandler={deleteHandler} editHandler={editHandler} />
+        <Items items={items} deleteHandler={deleteHandler} editHandler={editHandler} textEdit={textEdit} setTextEdit={setTextEdit} isEdit={isEdit} setIsEdit={setIsEdit} checkHandler={checkHandler} />
     </div>
   ) : <h1>Loading...</h1>
   
